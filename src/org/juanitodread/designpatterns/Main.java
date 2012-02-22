b5c47093eb966338864b52a8575383135e42debc
juanitodread <juanitodread@gmail.com>
2012-02-21 20:22:14 -0700

    Prueba desde intellij

--- src/org/juanitodread/designpatterns/Main.java
+++ src/org/juanitodread/designpatterns/Main.java
@@ -6,6 +6,6 @@ package org.juanitodread.designpatterns;
  */
 public class Main {
     public static void main(String... args){
-        System.out.println("Cada patrón de diseño cuenta con su método main.");
+        System.out.println("Cada patrón de diseño cuenta con su método main. ");
     }
 }
